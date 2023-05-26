f75fcc155c594ceb32622cf15719669614e6f29e
John Michael Sullivan <johnmichael.sullivan@ldas-grid.ligo.caltech.edu>
2023-05-25 22:57:10 -0700

    fixed comments

--- recolorimage.py
+++ recolorimage.py
@@ -20,11 +20,11 @@ def load_and_recolor_image(image_path, new_cmap='hot'):
     - If they are connected to "sure-edge" pixels, they are considered to be part of edges. 
     - Otherwise, they are also discarded.
     '''
-    
+    ### hand tune edge thresholds for individual use case
     edge_threshold1 = 500
     edge_threshold2 = 1250
     
-    # Now, load the original image (read in grayscale)
+    # Load original image (read in grayscale)
     img_original = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
 
     # Normalize image to 0-1 range (float)
@@ -34,43 +34,41 @@ def load_and_recolor_image(image_path, new_cmap='hot'):
         # generate colormap
         cmap = plt.get_cmap(new_cmap)
     except Exception:
-        # if invalid colormap name entered, throw:
+        # if invalid colormap name entered for mpl, throw:
         print('please enter a valid colormap name.')
-        sys.exit(1)    
+        sys.exit(1) 
+        ### (see also docs for alternatives mentioned in README.md)   
 
-    # Normalize grayscale image to the range of non-black/white colors in the colormap
-    img_norm = img_original.astype(float) / 255
-    ### OLD:
-    # img_norm = (img_norm - min_x) / (max_x - min_x)
-
-    # Convert grayscale image to colormap using matplotlib
+    # map grayscale image to recoloring using matplotlib colormap
     colored_image = cmap(img_norm)
 
-    # But first convert to float32 as cvtColor expects float data to be 32-bit
+    # convert to float32 for cvtColor method; it expects floats to be 32-bit
     colored_image = colored_image.astype(np.float32)
 
-    # Matplotlib returns rgba images, we need to convert to rgb
+    # matplotlib gives rgba images; we need to back in rgb
     colored_image_rgb = cv2.cvtColor(colored_image, cv2.COLOR_RGBA2RGB)
     
-    # Convert back to 0-255 range (uint8)
+    # convert back to 0-255 pixel values for color (uint8)
     colored_image_rgb = (colored_image_rgb * 255).astype(np.uint8)
-    # colored_image_rgb[img_original < 10] = 0
     
-    # Detect edges in the original grayscale image
+    # detect edges in the grayscale image conversion
     edges = cv2.Canny(img_original, edge_threshold1, edge_threshold2)
     
-    # Dilate the edges to create a mask for areas near high contrast borders
+    # dilate the edges to create a blur for areas near high contrast (steep edge gradient) borders (OPTIONAL: DEPENDING ON DESIRED RESOLUTION)
     dilated_edges = cv2.dilate(edges, np.ones((3, 3), np.uint8))
 
-    # Create a 3-channel version of the mask to apply on the rgb image
+    # make 3-channel (R,G,B) version of the mask to apply on the rgb image
     edge_mask_rgb = np.stack([dilated_edges]*3, axis=-1)
 
-    # Wherever the mask is true (near edges), restore the original grayscale color in the colored image
+    # [OPTIONAL] edge mask applies near sharp edges, there: restore the original grayscale color in the colored image.
+    ### Effect: keep sharp black and white outlines in current configuration. 
+    ### BUT, also have some around i.e. spectrographic (blurred) features if not careful with thresholds. 
     img_original_rgb = cv2.cvtColor(img_original, cv2.COLOR_GRAY2RGB)
 
+    # apply selected pixels from last step to (already) recolored pixels to "retrace solid outlines" of image features in B&W
     colored_image_rgb[edge_mask_rgb > 0] = img_original_rgb[edge_mask_rgb > 0]
 
-    # assure that white and black colors stay the same 
+    # (probably redundant) Floor white and black colors to those corresponding pixel values. 
     colored_image_rgb[img_original == 0] = 0
     colored_image_rgb[img_original == 255] = 255
 
@@ -102,6 +100,10 @@ if __name__ == "__main__":
         
         cv2.imwrite(outPath, cv2.cvtColor(colored_image, cv2.COLOR_RGB2BGR))
     else: 
+        '''
+        if we want to see samples of all colormaps which we could use from the above list on our image data,
+        use --test True in the command line! Outputs all to same directory as input image (FIXME).
+        '''
         for c in cmap_list:
             colored_image = load_and_recolor_image(image_path, c)
 
